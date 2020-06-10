37d1925a01e1ecfd794dac5dfd680f5b8e3a703d
Cocido <devmatiasbritez@gmail.com>
2020-06-10 00:16:09 -0300

    11

--- kiosko/kiosko/wsgi.py
+++ kiosko/kiosko/wsgi.py
@@ -8,14 +8,9 @@ https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
 """
 
 import os
-import sys
-
-path = os.path.expanduser('~/Kiosko-pagina-web')
-if path not in sys.path:
-    sys.path.append(path)
-
-os.environ['DJANGO_SETTINGS_MODULE'] = 'kiosko.settings'
 
 from django.core.wsgi import get_wsgi_application
-from django.contrib.staticfiles.handlers import StaticFilesHandler
-application = StaticFilesHandler(get_wsgi_application())
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'kiosko.settings')
+
+application = get_wsgi_application()
